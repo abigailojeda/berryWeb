f750bc512ad93b530a95eb34e46fb236005f5b0c
abigail.ojeda <abigail.ojeda@bitboxonline.com>
2023-01-03 15:44:28 +0000

    layout

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -10,12 +10,14 @@ export class HomePageComponent implements OnInit {
 
   public logoWhite:string='' ;
   public headerIllustration:string='' ;
+  public footerIllustration:string='' ;
 
   constructor() { }
 
   ngOnInit(): void {
     this.logoWhite = 'assets/img/logo/logoBerryWhiteLarge.svg';
     this.headerIllustration = 'assets/img/illustrations/berry_header_illustration.svg';
+    this.footerIllustration = 'assets/img/illustrations/berry_footer_illustration.svg';
   }
 
 }
