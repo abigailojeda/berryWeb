646925128f49850e17cbf1b3a9599de82cf0d720
abigail.ojeda <abigail.ojeda@bitboxonline.com>
2023-01-04 14:09:23 +0000

    AOS animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'berryweb';
+
+  ngOnInit(): void {
+   
+    AOS.init();
+    window.addEventListener('load', AOS.refresh)
+    
+  }
+
 }
